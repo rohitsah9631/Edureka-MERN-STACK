333cf90acaff83f0a63b4334474d784e256e5a94
rohitsah9631 <rsah48290@gmail.com>
2022-04-20 21:42:24 +0530

    array.js

--- javascript/array.js
+++ javascript/array.js
@@ -4,6 +4,7 @@
 
 //1. Creating the array 0  1  2  3  4  5
 const arrayofNumbers = [10,20,30,40,50,60];
+console.log(typeof arrayofNumbers);
 
 //2.How to access one value from array
 console.log(arrayofNumbers[1]);
@@ -44,3 +45,22 @@ arrayofNumbers.splice(4,0,44);
 console.log(arrayofNumbers);
 
 
+//10. How to get part of an array.
+
+const subArray = arrayofNumbers.slice(1,4);
+console.log(subArray);
+
+//11. how to find an element in array
+//[10, 11, 20, 22,44, 50, 60 ]
+const result = arrayofNumbers.find(e=> e==50);//find function return only one value.
+console.log(result);
+
+//12.How to find multiple element in array based on acondition?
+//[10, 11, 20, 22,44, 50, 60 ]
+const numbersGraterThan40 = arrayofNumbers.filter(e=> e>40);//filter function return multiple value(array of item).
+console.log(numbersGraterThan40);
+console.log(typeof numbersGraterThan40); 
+
+//13. how to find index of element in an array?
+const indexofaNumber = arrayofNumbers.findIndex(e=> e==44);
+console.log(indexofaNumber);
