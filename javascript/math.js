d924448816a4c72d1c183b1906f1446b2a3d3433
rohitsah9631 <rsah48290@gmail.com>
2022-04-22 01:30:33 +0530

    math.js

--- javascript/math.js
+++ javascript/math.js
@@ -1,4 +1,4 @@
-// math object in js.
+// math object in js. helps us to perform some mathematical operation in javascript.
 
 // How  to round a decimal value. (4.6)
 console.log(Math.round(4.6));//5
@@ -6,7 +6,7 @@ console.log(Math.round(4.6));//5
 console.log(Math.round(4.3));//4
 console.log(Math.round(4.5));//5
 
-//How to get nearest integer round up?
+//How to get nearest integer rounded up?
 console.log(Math.ceil(4.3));//5
 
 //How to get nearest integer round down?
