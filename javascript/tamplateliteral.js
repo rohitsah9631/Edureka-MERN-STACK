b933def9c9e73c137c1df7d8cc059f5c5b2b44c0
rohitsah9631 <rsah48290@gmail.com>
2022-04-22 00:55:08 +0530

    tamplateliteral.js

--- javascript/tamplateliteral.js
+++ javascript/tamplateliteral.js
@@ -1,13 +1,14 @@
 // tamplate Literal in js
 
 //Allows you to embaded js variable in string.
+// provides in ES6
 
 const name = "Rohit";
 const role = "student";
 const location ="indai";
 const exp = "0-2 year"
 
-//my name is rohitand i am a student. and i saty in india and i have 0-2years of exprience.
+//my name is rohit and i am a student. and i saty in india and i have 0-2 years of exprience.
 const sentence= "my name is "+name+" and i am a "+role+" and i stay in "+location+" and i have "+exp+" of exprience."
 console.log(sentence);
 
